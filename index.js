d1b271fa6ee877aef17fe0a461debc19c37176e5
naruedon wattanakul <naruedonw@gmail.com>
2020-01-28 00:43:17 +0700

    Removed Unused Code
    Signed-off-by: naruedon wattanakul <naruedonw@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-// A plugin is a Node module that exports a function which takes a `robot` argument
 module.exports = robot => {
 
   
@@ -15,25 +13,15 @@ module.exports = robot => {
       head: pr.head.sha
     }));
 
-    // const passCheck = compare.data.commits.every(data => {
-    //   return data.commit.message.match(/DevelopmentBranchTesting/);
-    // });
     var fullreponame = repo.full_name;
     var pullnum = pr.number;
     var finalurl = 'https://patch-diff.githubusercontent.com/raw/'+fullreponame+'/pull/'+pullnum+'.diff'
-    
-    //This needs to be reworked from issues to pull requst
-    //const autocomment = context.pulls({body: 'Probot will autocomment when running :D'})
-    //context.github.issues.createComment(autocomment)
-    
+        
     console.log(finalurl);
     var text;
     //async'ed for getting scripts
     (async (url) => {
       text = await getScript(url);
-      //Testing with only console log
-      //console.log(text+'+5555555');
-      //text = text + '+55555';
       var parse = require('parse-diff');
       var diff = text; // edit this to access the text on the internet site using POST or Get
       var files = parse(diff);
@@ -88,12 +76,7 @@ module.exports = robot => {
         "commitID":file.index[0].slice(9,16),
         "commitData":Commit
       }
-      PRWhole.push(pullRequestJSON);
-      //console.log(file.chunks.length); // number of chunks
-      //console.log(file.chunks[0].changes.length) // chunk added/deleted/context lines
-      //console.log(file.deletions); // number of deletions in the patch
-      //console.log(file.additions); // number of additions in the patch
-      
+      PRWhole.push(pullRequestJSON);      
       
   });
   console.log(JSON.stringify(PRWhole[0].commitData));
@@ -101,7 +84,7 @@ module.exports = robot => {
 
   const { exec } = require("child_process");
   //move this up top later with predefined var's 
-  /*var val = "ping www.google.com";
+  var val = "ping www.google.com";
   
   exec(val, (error, stdout, stderr) => {
       if (error) {
@@ -113,46 +96,7 @@ module.exports = robot => {
           return;
       }
       console.log(`stdout: ${stdout}`);
-  });*/
-
-    // Parameters for the status API
-    /*const params = {
-      sha: pr.head.sha,
-      context: 'Pass Checked',
-      state: passCheck ? 'success' : 'failure',
-      description: `Your commits ${passCheck ? 'have' : 'have not'} passed all the checks`
-    }*/
-    //New Params for issues
-    /*const params = {
-      
-      title: 'Issue Created on PullRQ',
-      body: 'Automatically Generated',
-      
-    }*/
-    /*const params = {
-      owner: 'AGS48353',
-      repo: 'TestingRepoNothing',
-      path: 'README.md'
-    }*/
-    
-
-    // Create the status
-    //return context.github.repos.createStatus(context.repo(params));
-    //return context.github.issues.create(context.repo(params));
-
-    /*context.github.repos.getContents({
-      owner: 'AGS48353',
-      repo: 'TestingRepoNothing',
-      path: 'BST/insert.java'
-    })
-    
-      .then(result => {
-        // content will be base64 encoded
-        const content = Buffer.from(result.data.content, 'base64').toString()
-        console.log(content)
-      })*/
-
-  
+  });
     
   });
 };
