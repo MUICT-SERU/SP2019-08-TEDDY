5c0a63c5040dbdb72a30741c97bcbc0d48c2b043
Phancisth <purit.pha@gmail.com>
2020-02-09 19:59:39 +0700

    Classes for testing/studying the deserialization from JSON string to java object
    Experimental classes, not used in actual operation of Teddy

---
+++ webapp/src/main/java/com/siamesex/standalone/model/Item.java
@@ -0,0 +1,28 @@
+package com.siamesex.standalone.model;
+
+public class Item {
+
+    public String name;
+    public int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
