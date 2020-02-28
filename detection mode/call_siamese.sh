588af6ec73afabe21cb956ae26958845944473f9
Phancisth <purit.pha@gmail.com>
2020-02-28 18:48:38 +0700

    Update call_siamese.sh

--- detection mode/call_siamese.sh
+++ detection mode/call_siamese.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-#receiving of variable arguments to be revised (how to get inputPath and outputPath from DON's part)
+# This shell must receive the path of where the GitHub is cloned and stored in $cloneRepoPath
+
 
 idiomPath = "D:\senior-project\python3-idiom"
 
+# Reading of $indexName variable is optional if Don's script can get ES index's name from the user already
 echo "Enter index name for your repository"
 read indexName
 
-#Index the cloned repo
+# Indexing the cloned repo
 java -jar siamese.jar -cf ./config.properties -c index -i $cloneRepoPath -n $indexName
 
-#Search using the idioms
+# Searching in the newly created ES index using the idiom/non-idiom Python snippets
+# '-n' is a new command-line overriding parameter of .jar for index in config.properties
 java -jar siamese.jar -cf ./config.properties -c search -i $idiomPath -o $outputResultPath -n $indexName
 
 echo "Search completed"
