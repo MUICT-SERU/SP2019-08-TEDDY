079a9c044fefadb7e2d5eb342de6b815d8f3f706
Phancisth <purit.pha@gmail.com>
2020-03-09 18:13:57 +0700

    Small change on the shell script

--- detection mode/call_siamese.sh
+++ detection mode/call_siamese.sh
@@ -5,6 +5,9 @@
 # $1 - path to the cloned repository
 # $2 - path to the idiom/non-idiom snippets
 # $3 - path for the output files to be saved
+# $4 - name for the cloned repository Elasticsearch index
+# $5 - commitID
+# $6 - version tracking num (start from 1) of the cloned repo
 
 idiomPath = "D:\senior-project\python3-idiom"
 
@@ -14,11 +17,11 @@ read indexName
 
 
 # Indexing the cloned repo
-java -jar siamese.jar -cf ./config.properties -c index -i $1 -n $indexName
+java -jar siamese.jar -cf ./config.properties -c index -i $1 -n $4
 
 # Searching in the newly created ES index using the idiom/non-idiom Python snippets
 # '-n' is a new command-line overriding parameter of .jar for index in config.properties
-java -jar siamese.jar -cf ./config.properties -c search -i $2 -o $3 -n $indexName
+java -jar siamese.jar -cf ./config.properties -c search -i $2 -o $3 -n $4
 
 echo "Search completed"
 
