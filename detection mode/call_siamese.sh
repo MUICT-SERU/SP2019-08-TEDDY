6dfca293b8b560849b5972f3f36b9d79e369891d
Phancisth <purit.pha@gmail.com>
2020-03-10 20:35:44 +0700

    Update call_siamese.sh
    the siamese.jar can receive the commitID and commitTrack for using in the new naming pattern for detection mode visualization

--- detection mode/call_siamese.sh
+++ detection mode/call_siamese.sh
@@ -2,12 +2,12 @@
 # This shell must receive the path of where the GitHub is cloned and stored in $cloneRepoPath
 #
 # Command-line arguments
-# $1 - path to the cloned repository
-# $2 - path to the idiom/non-idiom snippets
+# $1 - path to the cloned repository folder
+# $2 - path to the idiom/non-idiom snippets folder
 # $3 - path for the output files to be saved
-# $4 - name for the cloned repository Elasticsearch index
+# $4 - name for the Elasticsearch index of the cloned repository 
 # $5 - commitID
-# $6 - version tracking num (start from 1) of the cloned repo
+# $6 - commit tracking number (starting from 1) of the cloned repo
 
 idiomPath = "D:\senior-project\python3-idiom"
 
@@ -21,7 +21,7 @@ java -jar siamese.jar -cf ./config.properties -c index -i $1 -n $4
 
 # Searching in the newly created ES index using the idiom/non-idiom Python snippets
 # '-n' is a new command-line overriding parameter of .jar for index in config.properties
-java -jar siamese.jar -cf ./config.properties -c search -i $2 -o $3 -n $4
+java -jar siamese.jar -cf ./config.properties -c search -i $2 -o $3 -n $4 -g $5 -t $6
 
-echo "Search completed"
+echo "Commit #" + $6 + " ID:" + $5 + " search completed"
 
