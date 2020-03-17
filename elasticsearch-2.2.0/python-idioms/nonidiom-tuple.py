8fcf74373c3a6533917eb2155fc59d2e6a60d4c3
Phancisth <purit.pha@gmail.com>
2020-03-17 23:40:24 +0700

    Adding new snippets for idiomatic/non-idiomatic sample codes

---
+++ elasticsearch-2.2.0/python-idioms/nonidiom-tuple.py
@@ -0,0 +1,46 @@
+def n48():
+    list_from_comma_separated_value_file = ['dog', 'Fido', 10]
+    animal = list_from_comma_separated_value_file[0]
+    name = list_from_comma_separated_value_file[1]
+    age = list_from_comma_separated_value_file[2]
+
+def n49():
+    catherine_info = ['Catherine', 1960, 'Australian', 'F', 165, 50, 'Trainee']
+    class Staff:
+        name = ''
+        year-of-birth = 0
+        nationality = ''
+        gender = ''
+        height = 0
+        weight = 0
+        position = ''  
+    cat = Staff()
+    cat.name = catherine_info[0]
+    cat.year-of-birth = catherine_info[1]
+    cat.nationality = catherine_info[2]
+    cat.gender = catherine_info[3]
+    cat.height = catherine_info[4]
+    cat.weight = catherine_info[5]
+    cat.position = catherine_info[6]
+
+def n50():
+    DOB_numbers = [11,27,1,9,1996]
+    h = DOB_numbers[0]
+    m = DOB_numbers[1]
+    D = DOB_numbers[2]
+    M = DOB_numbers[3]
+    Y = DOB_numbers[4]
+    return '%i:%i %i-%i-%i' % (h,m,D,M,Y)
+
+def n51():
+    blood_group = ['A','B','O','B']
+    class Person:
+        blood = 'X'
+    p1 = Person()
+    p1.blood = blood_group[0]
+    p2 = Person()
+    p2.blood = blood_group[1]
+    p3 = Person()
+    p3.blood = blood_group[2]
+    p4 = Person()
+    p4.blood = blood_group[3]
