51b521eac1b09f0a4265e34bbbc04acd19966d04
Phancisth <purit.pha@gmail.com>
2020-04-24 15:16:43 +0700

    Small patch update in the wrong ordering sequemce of commit no

--- detection-mode/AutoRun.sh
+++ detection-mode/AutoRun.sh
@@ -20,6 +20,7 @@ cd */.
 gitclonedhead=$(git symbolic-ref --short HEAD)
 commit=$(git log --oneline --pretty=format:"%h;")
 commitarray=(${commit//;/})
+commitNo=0
 for ((i=${#commitarray[@]};i>0;i--))
 do
 echo "Commit#$i ID:${commitarray[$i]} >> Starting search"
@@ -27,13 +28,14 @@ echo "Commit#$i ID:${commitarray[$i]} >> Starting search"
 git checkout ${commitarray[$i]}
 # Calling shell script that runs Siamese.jar, perform indexing and searching
 #=====================================
-./../CallSiamese.sh $PWD ./../../python-idioms $2 $3 ${commitarray[$i]} $i
+./../CallSiamese.sh $PWD ./../../python-idioms $2 $3 ${commitarray[$i]} $commitNo
 #=====================================
 
-## Uncomment/Comment for manual/auto iteration of commit versions
-	#echo "Press any Key to Continue to the next commit"
-	#read -n 1
+  ## Uncomment/Comment for manual/auto iteration of commit versions
+  ##echo "Press any Key to Continue to the next commit"
+  ##read -n 1
 git checkout $gitclonedhead
+((commitNo=commitNo+1))
 done
 ## Deleting the repo folder after iteration is completed
 cd ..
